95107efe2a07020220a4c333d0d93e4a4cb4b0f7
shweta hasani <95736603+Shwetahasani@users.noreply.github.com>
2023-10-24 20:14:25 +0530

    Delete src directory

--- src/components/Result.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Result = ({ selectedCityName, selectedStateName }) => {
-  return (
-    <div>
-      You Have selected {selectedCityName}, {selectedStateName}
-    </div>
-  );
-};
